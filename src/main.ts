5dabf7653b9c5495ae8c0a0f0494b2c3d116c8c6
ngduytradescartes <95331134+ngduytradescartes@users.noreply.github.com>
2022-10-10 02:41:05 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+  });
   app.use(cookieParser());
   app.use(morgan('tiny'));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
